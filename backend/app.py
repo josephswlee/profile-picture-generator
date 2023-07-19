19000afc3ebf4c28b3413347833db5b476f7543f
Joseph Lee <josephslee9202@gmail.com>
2023-07-19 16:17:48 -0400

    html modified

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,14 @@ from PIL import Image
 
 app = Flask(__name__, static_folder='../frontend/static', template_folder='../frontend/templates')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+def home():
+    return render_template('home.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
     try:
-        data = request.form['prompts']
+        data = request.form['positivePrompts']
         image = Image.new('RGB', (100, 100), color='red')
 
         image_stream = io.BytesIO()
@@ -20,6 +20,10 @@ def generate():
         return send_file(image_stream, mimetype='image/png')
     except Exception as e:
         return jsonify({'error': str(e)})
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
