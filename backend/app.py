7ec1cb8fdf8d9930dad86d58d60ae8a79db55362
Joseph Lee <josephslee9202@gmail.com>
2023-07-23 14:35:36 -0400

    token embedding function added

--- backend/app.py
+++ backend/app.py
@@ -48,10 +48,47 @@ pipeline.to("cuda")
 # pipeline.scheduler = UniPCMultistepScheduler.from_config(pipeline.scheduler.config)
 pipeline.load_lora_weights('.', weight_name="mix4.safetensors", local_files_only=True)
 
+def get_pipeline_embeds(pipeline, prompt, negative_prompt, device):
+    """ Get pipeline embeds for prompts bigger than the maxlength of the pipe
+    :param pipeline:
+    :param prompt:
+    :param negative_prompt:
+    :param device:
+    :return:
+    """
+    max_length = pipeline.tokenizer.model_max_length
+
+    # simple way to determine length of tokens
+    count_prompt = len(prompt.split(" "))
+    count_negative_prompt = len(negative_prompt.split(" "))
+
+    # create the tensor based on which prompt is longer
+    if count_prompt >= count_negative_prompt:
+        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+        shape_max_length = input_ids.shape[-1]
+        negative_ids = pipeline.tokenizer(negative_prompt, truncation=False, padding="max_length",
+                                          max_length=shape_max_length, return_tensors="pt").input_ids.to(device)
+
+    else:
+        negative_ids = pipeline.tokenizer(negative_prompt, return_tensors="pt", truncation=False).input_ids.to(device)
+        shape_max_length = negative_ids.shape[-1]
+        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False, padding="max_length",
+                                       max_length=shape_max_length).input_ids.to(device)
+
+    concat_embeds = []
+    neg_embeds = []
+    for i in range(0, shape_max_length, max_length):
+        concat_embeds.append(pipeline.text_encoder(input_ids[:, i: i + max_length])[0])
+        neg_embeds.append(pipeline.text_encoder(negative_ids[:, i: i + max_length])[0])
+
+    return torch.cat(concat_embeds, dim=1), torch.cat(neg_embeds, dim=1)
+
 def run_inference(positive_prompt, negative_prompt):
     image = pipeline(
-    prompt=positive_prompt,
-    negative_prompt=negative_prompt,
+    prompt_embeds=positive_prompt,
+    negative_prompt_embeds=negative_prompt,
+    # prompt=positive_prompt,
+    # negative_prompt=negative_prompt,
     width=512,
     height=512,
     num_inference_steps=25,
@@ -74,8 +111,19 @@ def generate():
 
         positive_prompt = request.form["positivePrompts"]
         negative_prompt = request.form["negativePrompts"]
+        # app.logger.info("Image generation started.")
+        # image = run_inference(positive_prompt, negative_prompt)
+        # app.logger.info("Image generation done.")
+
+        app.logger.info("Image embedding started.")
+        positive_embeds, negative_embeds = get_pipeline_embeds(pipeline, 
+                                                               positive_prompt,
+                                                               negative_prompt,
+                                                               "cuda")
+        app.logger.info("Image embedding done.")
+
         app.logger.info("Image generation started.")
-        image = run_inference(positive_prompt, negative_prompt)
+        image = run_inference(positive_embeds, negative_embeds)
         app.logger.info("Image generation done.")
 
         img_data = io.BytesIO()
